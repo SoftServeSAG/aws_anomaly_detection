6f9e081e0b01e0aa3c18f1e913f6c4358c9ff10b
Dmytro Zikrach <zikrach.dm@gmail.com>
2017-10-17 13:27:47 +0300

    add function for remove NAs values

---
+++ functions/remove_na_values.R
@@ -0,0 +1,57 @@
+require(xts)
+require(lubridate)
+require(dplyr)
+
+
+#Data Preprocessing Functionality: Data Imputation and Denoising #1
+
+remove_na_from_data <- function(data, type='zero'){
+    # Function replace NA values on 0, mean,...
+    #
+    # Input:
+    # data - dataframe with time-series data
+    # type - type of method ('none' - without changes, 
+    #       'zero' - replace NA to 0, 
+    #       'linear' - using linear interpolation,
+    #       'mean' - replase NA to mean of all time series data)
+    #
+    # Output:
+    # data without NA values
+    
+    # Change type of input data to xts
+    
+    if (!is.xts(data)){
+        data <- as.xts(data)
+    }
+    
+    # remove NAs values from either sides
+    data <- zoo::na.trim(object = data, sides = "both")
+    
+    # replace NA to 0
+    switch(type,
+           'zero' = {
+                data[is.na(data)] <- 0
+            },
+           'mean' = {
+                mean_data <- mean(data, na.rm=TRUE)
+                data[is.na(data)] <- mean_data
+           },
+           'linear' = {
+               data <- na.approx(data)
+           }
+    )
+    
+    return(as.data.frame(data))
+}
+
+# testing
+
+# n = 15
+# full.dates = seq(Sys.Date(), by = 'day', length = n)
+# y = rnorm(n)
+# serie = zoo(y, full.dates)
+# data <- as.xts(serie)
+# data[c(2, 6:7, 10)] <- NA
+# 
+# remove_na_from_data(data, type = 'linear')
+
