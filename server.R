c770a970784eb6aa381298da2fcf9a68b142055b
Volodymyr Solskyy <volodymyr.solskyy@gmail.com>
2017-11-14 14:55:09 +0200

    modified UI to accomodate recent changes

--- server.R
+++ server.R
@@ -9,78 +9,163 @@ source("src/aggregation.R")
 source("src/visualization.R")
 
 shinyServer(function(input, output, session) {
-    # data placeholders
-    data <- reactiveValues(
-                full = NULL,
-                series = NULL,
-                seriesNoMis = NULL,
-                seriesNoOut = NULL,
-                seriesNoNs = NULL,
-                seriesFinal = NULL,
-                aggData = NULL)
     
-    # FIRST (LOAD) PAGE CONTENT
-    # read uploaded file and render contents into a table
-    output$UserData <- DT::renderDataTable({
-        req(input$LocalFile)
-        data$full <- read.csv(
+    # GENERAL DATA PLACEHOLDERS
+    data <- reactiveValues(
+        orig = NULL,
+        series = NULL,
+        aggData = NULL)
+    
+    # TRAIN DATA PLACEHOLDERS
+    train <- reactiveValues(
+        series = NULL,
+        seriesNoMis = NULL,
+        seriesNoOut = NULL,
+        seriesNoNs = NULL,
+        seriesFinal = NULL
+    )
+    
+    # TEST DATA PLACEHOLDERS
+    test <- reactiveValues(
+        series = NULL,
+        seriesNoMis = NULL,
+        seriesNoOut = NULL,
+        seriesNoNs = NULL,
+        seriesFinal = NULL
+    )
+    
+    # FIRST PAGE CONTENT
+    # show modal dialog to select columns roles after file is loaded
+    observeEvent(input$LocalFile, {
+        # read and store uploaded data
+        data$orig <- read.csv(
             input$LocalFile$datapath,
             header = input$FileHasHeader,
             sep = input$FileSeparator,
             quote = input$FileQuotation,
             stringsAsFactors = F
         )
-        return(data$full)
+        # role selection dialog
+        showModal(
+            modalDialog(
+                DT::dataTableOutput("UserData"),
+                hr(),
+                fluidRow(
+                    column(6,
+                        fluidRow(
+                            column(6,
+                                div("Time variable:", style = "padding: 5px;font-size: 110%;")
+                            ),
+                            column(6,
+                                uiOutput("DateTimeVarSelector")
+                            )
+                        ),
+                        fluidRow(
+                            column(6,
+                                tags$div("Target variable:", style = "padding: 5px;font-size: 110%;")
+                            ),
+                            column(6,
+                                uiOutput("TargetVarSelector")
+                            )
+                        )
+                    ),
+                    column(6,
+                        fluidRow(
+                            column(6,
+                                div("Datetime format:", style = "padding: 5px;font-size: 110%;")
+                            ),
+                            column(6,
+                                textInput("DateTimeFmt", NULL, placeholder = "HH:MM:SS"),
+                                bsPopover("DateTimeFmt", "Datetime format", "Specify datetime format for time scale", placement="top")
+                            )
+                        )
+                    )
+                ),
+                title="Role assignment", 
+                size = "l", 
+                footer = shinyjs::disabled(actionButton(inputId = "RoleSelectBtn", "Select"))
+            )
+        )
+    })
+    
+    output$UserData <- DT::renderDataTable({
+        req(data$orig)
+        return(data$orig)
     },
     selection = 'none',
-    options = list(dom = 'tsp'))
+    options = list(dom = 'tsp', pageLength = 5, scrollX = T))
     
-    # store 'data loaded' flag in outputs for dynamic UI
-    output$UserDataLoaded <- reactive({
-        !is.null(data$full)
-    })
-    outputOptions(output, "UserDataLoaded", suspendWhenHidden = FALSE)
-    
-    output$DateTimeVarName <- renderUI({
-        selectizeInput("DateTimeVar", NULL, choices = names(data$full),
-            options = list(
-                placeholder = "Select",
-                onInitialize = I('function() { this.setValue(""); }')
-            )
+    output$DateTimeVarSelector <- renderUI({
+        selectizeInput("DateTimeVar", NULL, choices = names(data$orig),
+                       options = list(
+                           placeholder = "Select",
+                           onInitialize = I('function() { this.setValue(""); }')
+                       )
         )
     })
     
-    output$TargetVarName <- renderUI({
+    output$TargetVarSelector <- renderUI({
         selectizeInput("TargetVar", NULL,
-            choices = names(data$full[sapply(data$full, is.numeric)]),
-            options = list(
-                placeholder = "Select",
-                onInitialize = I('function() { this.setValue(""); }')
-            )
+                       choices = names(data$orig[sapply(data$orig, is.numeric)]),
+                       options = list(
+                           placeholder = "Select",
+                           onInitialize = I('function() { this.setValue(""); }')
+                       )
         )
     })
     
-    # process next button click (slice dataframe into series and move to the Prepare stage)
-    observeEvent(input$Next1Btn, {
+    output$DataSplitControl <- renderUI({
+        if(!is.null(data$series)) {
+            sliderInput("TrainTestSplit", "Data Split", min = 1, max = 100, step = 1, value = 67)
+        }
+    })
+    
+    output$NextBtn1Control <- renderUI({
+        if(!is.null(data$series)) {
+            fluidRow(
+                column(1, offset = 11,
+                    actionButton("Next1Btn", "Next", width = "100%")
+                )
+            )
+        }
+    })
+    
+    observeEvent(c(input$DateTimeVar, input$TargetVar, input$DateTimeFmt), {
+        if (input$DateTimeVar != "" && input$TargetVar != "" && input$DateTimeFmt != "") {
+            shinyjs::enable("RoleSelectBtn")
+        }
+    })
+    
+    observeEvent(input$RoleSelectBtn, {
         withProgress(message = "", value = 0, style = "old", {
-            data$series <- data$full[, c(input$DateTimeVar, input$TargetVar)]
+            data$series <- data$orig[, c(input$DateTimeVar, input$TargetVar)]
             data$series[, input$DateTimeVar] <-
                 strptime(data$series[, input$DateTimeVar], format = input$DateTimeFmt) %>% as.POSIXct()
             data$series <- xts(data$series[, 2], order.by = data$series[, 1])
             data$aggData <- timeSliders(data$series[, 1])
-            updateTabsetPanel(session, "MNB", selected = "Prepare")
         })
+        removeModal()
     })
     
-    observeEvent(c(input$DateTimeVar, input$TargetVar), {
-        if (input$DateTimeVar != "" && input$TargetVar != "") {
-            shinyjs::enable("Next1Btn")
+    output$OriginalSeries <- renderDygraph({
+        if(is.null(data$series) || is.null(input$TrainTestSplit)) {
+            return(NULL)
+        } else {
+            tts = input$TrainTestSplit / 100
+            return(plot_time_series(data$series, window_size = 0.01, train_test_split = tts))
         }
     })
     
+    observeEvent(input$Next1Btn, {
+        split.coef = input$TrainTestSplit / 100
+        train$series <- data$series[1:(floor(length(data$series) * split.coef))]
+        test$series <- data$series[(floor(length(data$series) * split.coef)+1):length(data$series)]
+        updateTabsetPanel(session, "MNB", selected = "Prepare")
+    })
+    
     # SECOND (PREPARE) PAGE CONTENT
-    # plot original series
-    output$OriginalSeries <- renderDygraph(plot_time_series(data$series))
+    # plot series, train part
+    output$TrainSeriesClean <- renderDygraph(plot_time_series(train$series))
     
     # aggregation dynamic UI parts
     output$AggUnitPlaceholder <- renderUI({
@@ -101,13 +186,13 @@ shinyServer(function(input, output, session) {
     # calculate results and add dynamic container for display
     observeEvent(input$ApplyTsfBtn, {
         withProgress(message = "", value = 0, style = "old", {
-            data$seriesNoMis <- remove_na_from_data(data$series, type = input$MissValTreatment)
-            data$seriesNoOut <-
-                remove_outliers_from_data(data$seriesNoMis, type = input$OutliersTreatment, number = input$OutliersSigmaCount)
+            train$seriesNoMis <- remove_na_from_data(train$series, type = input$MissValTreatment)
+            train$seriesNoOut <-
+                remove_outliers_from_data(train$seriesNoMis, type = input$OutliersTreatment, number = input$OutliersSigmaCount)
             win_ns <- if(input$NoiseWindowType != 'auto') input$NoiseWindowSize else input$NoiseWindowType
-            data$seriesNoNs <- denoise_data(data$seriesNoOut, type = input$NoiseTreatment, window_noise = win_ns)
+            train$seriesNoNs <- denoise_data(train$seriesNoOut, type = input$NoiseTreatment, window_noise = win_ns)
             agg_type <- if(input$AggFunction != 'none') paste(input$AggCount, input$AggUnit, sep = " ") else input$AggFunction
-            data$seriesFinal <- aggregation_data(data$seriesNoNs, type = agg_type, func_aggregate = input$AggFunction)
+            train$seriesFinal <- aggregation_data(train$seriesNoNs, type = agg_type, func_aggregate = input$AggFunction)
             
             removeUI(selector = "#processed-results")
             insertUI(
--- ui.R
+++ ui.R
@@ -6,190 +6,155 @@ library(dygraphs)
 shinyUI(
     fluidPage(
         tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "shiny.css")
+            tags$link(rel = "stylesheet", type = "text/css", href = "shiny.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "ui.css")
         ),
         shinyjs::useShinyjs(),
         navbarPage("Anomaly Detection", id = "MNB",
             tabPanel("Load",
                 fluidPage(
-                    fluidRow(
-                        column(3,
-                               h3("Load data"),
-                               br(),
-                               fileInput("LocalFile", NULL),
-                               hr(),
-                               h3("CSV options"),
-                               checkboxInput("FileHasHeader", "Header", TRUE),
-                               radioButtons(
-                                   "FileSeparator", "Separator",
-                                   choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
-                                   selected = ","
-                               ),
-                               radioButtons(
-                                   "FileQuotation", "Quote",
-                                   choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
-                                   selected = ""
-                               )
+                    sidebarLayout(
+                        sidebarPanel(id = "sidebar01",
+                            h3("Load data"),
+                            br(),
+                            fileInput("LocalFile", NULL),
+                            hr(),
+                            h3("CSV options"),
+                            checkboxInput("FileHasHeader", "Header", TRUE),
+                            radioButtons(
+                                "FileSeparator", "Separator",
+                                choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
+                                selected = ","
+                            ),
+                            radioButtons(
+                                "FileQuotation", "Quote",
+                                choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
+                                selected = ""
+                            ),
+                            width = 3
                         ),
-                        column(9,
-                               h2("Preview"),
-                               DT::dataTableOutput('UserData'),
-                               conditionalPanel(
-                                   condition = "output.UserDataLoaded",
-                                   fluidRow(
-                                       column(3,
-                                              h3("Time Scale"),
-                                              fluidRow(
-                                                  column(6,
-                                                         div("Column name", style = "padding: 5px;font-size: 110%;")
-                                                  ),
-                                                  column(6,
-                                                         uiOutput("DateTimeVarName")
-                                                  )
-                                              ),
-                                              fluidRow(
-                                                  column(6,
-                                                         div("Datetime format", style = "padding: 5px;font-size: 110%;")
-                                                  ),
-                                                  column(6,
-                                                         textInput("DateTimeFmt", NULL, placeholder = "HH:MM:SS"),
-                                                         bsPopover("DateTimeFmt", "Datetime format", "Specify datetime format for time scale", placement="top")
-                                                  )
-                                              )
-                                       ),
-                                       column(3,
-                                              h3("Target variable"),
-                                              fluidRow(
-                                                  # column(6,
-                                                  #        #tags$div("Column name", style = "padding: 5px;font-size: 110%;")
-                                                  # ),
-                                                  column(6,# offset = 6,
-                                                         uiOutput("TargetVarName")
-                                                  )
-                                              )
-                                       )
-                                   )
-                               )
-                        )
-                    )
-                ),
-                fluidRow(
-                    conditionalPanel(
-                        condition = "output.UserDataLoaded",
-                        column(1, offset = 11,
-                               shinyjs::disabled(actionButton("Next1Btn", "Next", width = '100%'))
+                        mainPanel(
+                            h3("Preview"),
+                            dygraphOutput("OriginalSeries"),
+                            hr(),
+                            uiOutput("DataSplitControl"),
+                            uiOutput("NextBtn1Control"),
+                            width = 9
                         )
                     )
                 )
             ),
             tabPanel("Prepare",
                 fluidPage(
-                    fluidRow(
-                        column(3,
-                            h3("Transformation"),
-                            hr(),
-                            fluidRow(
-                                column(6,
-                                    div(strong("Missing values"), style = "padding: 5px;font-size: 110%;")
-                                ),
-                                column(6,
-                                    selectInput("MissValTreatment", NULL, choices = c('Zero'='zero', 'Linear'='linear', 'Mean'='mean'))
-                                )
-                            ),
-                            hr(),
-                            fluidRow(
-                                column(6,
-                                    div(strong("Outliers Detection"), style = "padding: 5px;font-size: 110%;")
-                                ),
-                                column(6,
-                                    selectInput("OutliersTreatment", NULL, choices = c('None' = 'none', 'Normal'='normal', 'Diff'='diff'))
-                                )
-                            ),
-                            conditionalPanel(
-                                condition = "input.OutliersTreatment != 'none'",
-                                fluidRow(
-                                    column(6,
-                                        div("Count", style = "padding: 5px;font-size: 110%;")
+                    sidebarPanel(id = 'sidebar02',
+                        fluidRow(
+                            bsCollapse(open = "Transformation", multiple = F,
+                                bsCollapsePanel("Transformation",
+                                    fluidRow(
+                                        column(6,
+                                               div(strong("Missing values"), style = "padding: 5px;font-size: 110%;")
+                                        ),
+                                        column(6,
+                                               selectInput("MissValTreatment", NULL, choices = c('Zero'='zero', 'Linear'='linear', 'Mean'='mean'))
+                                        )
                                     ),
-                                    column(6,
-                                        sliderInput("OutliersSigmaCount", NULL, value = 5, min = 3, max = 7)
-                                    )
-                                )
-                            ),
-                            hr(),
-                            fluidRow(
-                                column(6,
-                                    div(strong("Noise Reduction"), style = "padding: 5px;font-size: 110%;")
-                                ),
-                                column(6,
-                                    selectInput("NoiseTreatment", NULL, choices = c('None' = 'none', 'Gaussian'='gaussian', 'SMA'='sma','EMA'='ema'))
-                                )
-                            ),
-                            conditionalPanel(
-                                condition = "input.NoiseTreatment != 'none'",
-                                fluidRow(
-                                    column(6,
-                                        div("Kernel size", style = "padding: 5px;font-size: 110%;")
+                                    hr(),
+                                    fluidRow(
+                                        column(6,
+                                               div(strong("Outliers Detection"), style = "padding: 5px;font-size: 110%;")
+                                        ),
+                                        column(6,
+                                               selectInput("OutliersTreatment", NULL, choices = c('None' = 'none', 'Normal'='normal', 'Diff'='diff'))
+                                        )
                                     ),
-                                    column(6,
-                                           selectInput("NoiseWindowType", NULL, choices = c('Auto'='auto', 'Manual'='manual'))
-                                    )
-                                )
-                            ),
-                            conditionalPanel(
-                                condition = "input.NoiseTreatment != 'none' && input.NoiseWindowType != 'auto'",
-                                fluidRow(
-                                    column(12,
-                                        sliderInput("NoiseWindowSize", NULL, value = 5, min = 3, max = 25, width = '100%')
-                                    )
-                                )
-                            ),
-                            hr(), hr(),
-                            h3("Aggregation"),
-                            hr(),
-                            fluidRow(
-                                column(6,
-                                    div(strong("Function"), style = "padding: 5px;font-size: 110%;")
-                                ),
-                                column(6,
-                                    selectInput("AggFunction", NULL, choices = c('None'='none', 'Sum'='sum', 'Min'='min', 'Avg'='mean', 'Median'='median', 'Max'='max'))
-                                )
-                            ),
-                            conditionalPanel(
-                                condition = "input.AggFunction != 'none'",
-                                fluidRow(
-                                    column(6,
-                                        div(strong("Unit"), style = "padding: 5px;font-size: 110%;")
+                                    conditionalPanel(
+                                        condition = "input.OutliersTreatment != 'none'",
+                                        fluidRow(
+                                            column(6,
+                                                   div("Count", style = "padding: 5px;font-size: 110%;")
+                                            ),
+                                            column(6,
+                                                   sliderInput("OutliersSigmaCount", NULL, value = 5, min = 3, max = 7)
+                                            )
+                                        )
                                     ),
-                                    column(6,
-                                        uiOutput("AggUnitPlaceholder")
+                                    hr(),
+                                    fluidRow(
+                                        column(6,
+                                               div(strong("Noise Reduction"), style = "padding: 5px;font-size: 110%;")
+                                        ),
+                                        column(6,
+                                               selectInput("NoiseTreatment", NULL, choices = c('None' = 'none', 'Gaussian'='gaussian', 'SMA'='sma','EMA'='ema'))
+                                        )
+                                    ),
+                                    conditionalPanel(
+                                        condition = "input.NoiseTreatment != 'none'",
+                                        fluidRow(
+                                            column(6,
+                                                   div("Kernel size", style = "padding: 5px;font-size: 110%;")
+                                            ),
+                                            column(6,
+                                                   selectInput("NoiseWindowType", NULL, choices = c('Auto'='auto', 'Manual'='manual'))
+                                            )
+                                        )
+                                    ),
+                                    conditionalPanel(
+                                        condition = "input.NoiseTreatment != 'none' && input.NoiseWindowType != 'auto'",
+                                        fluidRow(
+                                            column(12,
+                                                   sliderInput("NoiseWindowSize", NULL, value = 5, min = 3, max = 25, width = '100%')
+                                            )
+                                        )
                                     )
                                 ),
-                                fluidRow(
-                                    column(12,
-                                        uiOutput("AggCountPlaceholder")
+                                bsCollapsePanel("Aggregation",
+                                    fluidRow(
+                                        column(6,
+                                               div(strong("Function"), style = "padding: 5px;font-size: 110%;")
+                                        ),
+                                        column(6,
+                                               selectInput("AggFunction", NULL, choices = c('None'='none', 'Sum'='sum', 'Min'='min', 'Avg'='mean', 'Median'='median', 'Max'='max'))
+                                        )
+                                    ),
+                                    conditionalPanel(
+                                        condition = "input.AggFunction != 'none'",
+                                        fluidRow(
+                                            column(6,
+                                                   div(strong("Unit"), style = "padding: 5px;font-size: 110%;")
+                                            ),
+                                            column(6,
+                                                   uiOutput("AggUnitPlaceholder")
+                                            )
+                                        ),
+                                        fluidRow(
+                                            column(12,
+                                                   uiOutput("AggCountPlaceholder")
+                                            )
+                                        )
                                     )
                                 )
                             ),
                             fluidRow(
-                                column(4, offset=4,
-                                    actionButton("ApplyTsfBtn", "Apply", width = '100%')
+                                column(6, offset=3,
+                                       actionButton("ApplyTsfBtn", "Apply", width = '100%')
                                 )
                             )
                         ),
-                        column(9,
-                            wellPanel(id="PrepareResPanel", style = "overflow-y:scroll;height:80%;", #TODO: figure out height options
-                                      h3("Original"),
-                                      dygraphOutput("OriginalSeries"),
-                                      br(),
-                                      tags$div(id='tranform-res-placeholder')
-                            )
-                        )
+                        width = 3
                     ),
-                    fluidRow(
-                        column(1, offset=11,
-                            actionButton("Next2Btn", "Next", width = '100%')
-                        )
+                    mainPanel(
+                        wellPanel(id="PrepareResPanel", style = "overflow-y:scroll;height:80%;", #TODO: figure out height options
+                                   h3("Original"),
+                                   dygraphOutput("TrainSeriesClean"),
+                                   br(),
+                                   tags$div(id='tranform-res-placeholder')
+                        ),
+                        fluidRow(
+                            column(1, offset=11,
+                                   actionButton("Next2Btn", "Next", width = '100%')
+                            )
+                        ),
+                        width = 9
                     )
                 )
             ),
