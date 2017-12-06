5665ba2c072fc6a3ba5532019ce52d966534cc27
Iurii Milovanov <duruku@gmail.com>
2017-12-05 20:28:26 -0600

    added check&kill for running containers

--- dockerRun.sh
+++ dockerRun.sh
@@ -2,4 +2,8 @@
 APP_NAME=$1
 APP_PORT=$2
 
-docker run --rm -d --memory="2g" --memory-swap="1g" -p "${APP_PORT}:3838" "${APP_NAME}"
+if [ $(docker inspect -f '{{.State.Running}}' $APP_NAMR) = "true" ]; then
+  docker kill "${APP_NAME}"
+fi
+
+docker run --rm -d --memory="2g" --memory-swap="1g" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
