54e63ca5125c6dbe08c695b684283ce69554371d
Iurii Milovanov <duruku@gmail.com>
2017-12-05 20:37:27 -0600

    separate vars for resource conf

--- dockerRun.sh
+++ dockerRun.sh
@@ -2,8 +2,11 @@
 APP_NAME=$1
 APP_PORT=$2
 
+MEMORY="2g"
+SWAP_MEMORY="1g"
+
 if [ $(docker inspect -f '{{.State.Running}}' $APP_NAMR) = "true" ]; then
   docker kill "${APP_NAME}"
 fi
 
-docker run --rm -d --memory="2g" --memory-swap="1g" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
+docker run --rm -d --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
