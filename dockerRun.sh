34dc1b6bad02cb32fed808776f3dc24699c6aa17
Iurii Milovanov <duruku@gmail.com>
2018-04-16 11:16:05 -0500

    minor bugfixes for dockerRun.sh

--- dockerRun.sh
+++ dockerRun.sh
@@ -6,8 +6,8 @@ MEMORY="2g"
 SWAP_MEMORY="1g"
 CPUS="4"
 
-if [ $(docker inspect -f '{{.State.Running}}' $APP_NAMR) = "true" ]; then
+if [ $(docker inspect -f '{{.State.Running}}' $APP_NAME) = "true" ]; then
   docker kill "${APP_NAME}"
 fi
 
-docker run --rm -d --cpus="${CPUS}" --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
+docker run -d --cpus="${CPUS}" --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
