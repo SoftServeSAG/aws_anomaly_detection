87bdfaa2ff2c8a2119b1c13fea9fbc3e526f5f09
Iurii Milovanov <duruku@gmail.com>
2018-04-22 11:22:37 -0500

    fixed docker clean up procedure

--- dockerRun.sh
+++ dockerRun.sh
@@ -6,9 +6,8 @@ MEMORY="2g"
 SWAP_MEMORY="1g"
 CPUS="4"
 
-if [ $(docker inspect -f '{{.State.Running}}' $APP_NAME) = "true" ]; then
-  docker kill "${APP_NAME}"
-fi
+docker kill "${APP_NAME}" || true
+docker rm -f "${APP_NAME}" || true
 
 docker run -d --cpus="${CPUS}" --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
 docker update --restart always "${APP_NAME}"
